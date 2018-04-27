42e01c7aa1a53ef8440190837bfdae21394dd7b6
samrattennis <nallas@purdue.edu>
2018-04-26 22:34:17 -0400

    Game works for a 2x2 square

--- gamePage.js
+++ gamePage.js
@@ -24,9 +24,35 @@ function play2() {
 
 }
 
+function resetAnimation () {
+    var element = document.getElementById("rect1");
+    var tryAgainButton = document.getElementById("btnTryAgain");
+
+    tryAgainButton.addEventListener('click', function (e) {
+        e.preventDefault
+
+        element.classList.remove("rectangle1");
+        void element.offsetWidth
+        element.classList.add("rectangle1");
+    });
+}
+
+function reloadPlay(){
+    // checker = 1
+    window.location.reload();
+    // document.getElementById("rect1").style.display = 'block'
+    // console.log("reloading")
+    // $("#rect1").css({
+    //     '-webkit-animation-play-state': 'running'
+
+    // })
+    // startGame()
+}
+
 function play3() {
     console.log("starting game2")
-    // document.getElementById("rect1").style.display = 'block'
+
+
 
     $("#rect1").css({
         '-webkit-animation-play-state': 'running'
@@ -52,125 +78,15 @@ function play3() {
     //     'display': 'none'
 
     // })
-    startGame2()
+    startGame()
+    resetAnimation()
     console.log("yooooo")
-
-    
 }
 
-function startGame2() {
-    console.log("why you no work?")
 
-    var start = window.performance.now()
-    var color = ""
-    var end = 0
-    var spaceCheck = 0
-
-    var counter = 0;
-    $(body).keyup(function(e){
-        if (e.keyCode == 32) {
-            console.log("listened to spacebar")
-            if (counter === 0) {
-                $("#rect1").css({
-                    '-webkit-animation-play-state': 'paused'
-
-                })
-
-                color = $("#rect1").css("background-color")
-
-                // console.timeEnd()
-
-
-                $("#rect2").css({
-                    'display': 'block'
-                })
-                console.log("block 1 paused");
-                counter++;
-                console.log("counter: " + counter);
-            }
-            else if (counter === 1) {
-                $("#rect2").css({
-                    '-webkit-animation-play-state': 'paused'
-                })
-
-                var color2 = $("#rect2").css("background-color")
-
-                if (color == color2) {
-
-
-                $("#rect3").css({
-                    'display': 'block'
-                })
-                counter++;
-            } else {
-                // document.getElementById("btnPlay").innerHTML = "Play again"
-                if (!(spaceCheck >= 1)) {
-                end = window.performance.now()
-                end -= start
-                end /= 1000
-                console.log(end + " secs")
-                } 
-                spaceCheck++
-
-            }
-            }
-            else if (counter === 2) {
-                $("#rect3").css({
-                    '-webkit-animation-play-state': 'paused'
-                })
-
-                var color3 = $("#rect3").css("background-color")
-
-                if (color == color3) {
-
-                $("#rect4").css({
-                    'display': 'block'
-                })
-                counter++;
-            } else {
-                if (!(spaceCheck >= 1)) {
-                    // document.getElementById("btnPlay").innerHTML = "Play again"
-                    end = window.performance.now()
-                    end -= start
-                    end /= 1000
-                    console.log(end + " secs")
-                    }
-                    spaceCheck++
-
-            }
-            }
-            else if (counter === 3) {
-                $("#rect4").css({
-                    '-webkit-animation-play-state': 'paused'
-                })
-
-                var color4 = $("#rect4").css("background-color")
-
-                if (color == color4) {
-                    console.log("You win!")
-
-                } else {
-                    console.log("You lost :(")
-                }
-                if (!(spaceCheck >= 1)) {
-                    // document.getElementById("btnPlay").innerHTML = "Play again"
-                    end = window.performance.now()
-                    end -= start
-                    end /= 1000
-                    console.log(end + " secs")
-                    }
-                    spaceCheck++
-
-                // $("#rect4").css({
-                //     'display': 'block'
-                // })
-                counter++;
-            }
-
-        }
-    })
+if (checker == 1) {
+    startGame()
 }
-
 
 function startGame() {
     console.log("why you no work?")
@@ -193,8 +109,6 @@ function startGame() {
                 color = $("#rect1").css("background-color")
 
                 // console.timeEnd()
-
-
                 $("#rect2").css({
                     'display': 'block'
                 })
